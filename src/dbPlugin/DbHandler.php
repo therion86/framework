8dc297f1e3bdeec97ba6c1f30b553dee72548184
Therion86 <sascha.emmrich@gmail.com>
2022-01-09 12:06:13 +0100

    cleanup code in test-plugin

--- src/dbPlugin/DbHandler.php
+++ src/dbPlugin/DbHandler.php
@@ -7,26 +7,11 @@ class DbHandler {
 
 	private DbConnection $dbConnection;
 
-	private string $dsn;
-
-	private string $user;
-
-	private string $password;
-
-	public function __construct(string $dsn, string $user, string $password) {
-		$this->dsn = $dsn;
-		$this->user = $user;
-		$this->password = $password;
+    public function __construct(private string $dsn, private string $user, private string $password) {
         $this->dbConnection = new DbConnection($this->dsn, $this->user, $this->password);
 	}
 
 	public function getConnection(): DbConnection {
         return $this->dbConnection;
 	}
-
-	public function prepareStatement(string $statement): DbStatement {
-		return $this->getConnection()->prepare($statement);
-	}
-
-
 }
